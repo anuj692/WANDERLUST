e59fb7ca1356d7a63853e83121366aef9df3fa84
Akhil Jadli <akhiljadli82@gmail.com>
2024-10-02 13:31:53 +0530

    Your commit message

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsyc.js");
 const expressError = require("./utils/expressError.js");
-const {listingSchema} = require("./schema.js");
+const { listingSchema } = require("./schema.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -34,6 +34,17 @@ app.get("/", (req, res) => {
   res.send("Hi, I am root");
 });
 
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  let errMsg = error.details.map((el) => el.message).join(",");
+  if (error) {
+    throw new expressError(400, errMsg);
+  }
+  else {
+    next();
+  }
+}
+
 //Index Route
 app.get("/listings", wrapAsync(async (req, res) => {
   const allListings = await Listing.find({});
@@ -53,12 +64,8 @@ app.get("/listings/:id", wrapAsync(async (req, res) => {
 }));
 
 //Create Route
+
 app.post("/listings", wrapAsync(async (req, res, next) => {
-  let result  = listingSchema.validate(req.body);
-  console.log(result);
-  if(result.error){
-    throw new expressError(400,result.error);
-  }
   const newListing = new Listing(req.body.listing);
   await newListing.save();
   res.redirect("/listings");
@@ -72,10 +79,8 @@ app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
 }));
 
 // //Update Route
-app.put("/listings/:id", wrapAsync(async (req, res) => {
-  if(!req.body.listing){
-    throw new expressError(400,"Send valid data for listing");
-  }
+app.put("/listings/:id", validateListing, wrapAsync(async (req, res) => {
+
   let { id } = req.params;
   await Listing.findByIdAndUpdate(id, { ...req.body.listing });
   res.redirect(`/listings/${id}`);
@@ -112,7 +117,7 @@ app.all("*", (req, res, next) => {
 //middleware
 app.use((err, req, res, next) => {
   let { statusCode = 500, message = "Something went wrong" } = err;
-  res.status(statusCode).render("error.ejs",{message});
+  res.status(statusCode).render("error.ejs", { message });
 
   // res.status(statusCode).send(message);
 })
